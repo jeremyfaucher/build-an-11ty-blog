e0d588c1b3cc4d5d26cde5744e6efcdc6d346f24
Jeremy Faucher <cafefaucher@gmail.com>
2022-04-26 21:07:11 -0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,22 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
 
   //Pass through files
   eleventyConfig.addPassthroughCopy('./src/style.css');
+  eleventyConfig.addPassthroughCopy('./src/assets');
 
+  //Date Clean up
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
+  // Return your Object options:
   return {
-  	dir: {
-  		input: "src"
-  	}
+    dir: {
+      input: "src",
+      output: "_site",
+    }
   }
   
 };
